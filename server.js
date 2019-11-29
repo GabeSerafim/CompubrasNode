a32646bba6bf7fb9b1ee221aff3df32b27ef5554
gSerafim <gserafim@brazpine.com>
2019-11-29 15:14:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express');
+
+const root = require('./helpers/path');
+const routes = require('./routes/index.routes.js');
+
+const app = express();
+
+app.use(routes);
+app.use((req, res) => {
+    res.status(404)
+})
+app.listen(8080)
