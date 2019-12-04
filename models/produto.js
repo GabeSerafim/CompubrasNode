3d7c41e115ec2cbdb83d2c424e06561833a7462d
gSerafim <gserafim@brazpine.com>
2019-12-04 17:58:40 -0300

    feat(models): produto

---
+++ models/produto.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../utils/dataase');
+
+const Produto = sequelize.define('produto',{
+    CodProduto: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true
+    },
+    Descricao: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+    },
+    ValorUnitario: {
+        type: Sequelize.DECIMAL(10,2),
+        allowNull: false
+    }
+})
+
+module.exports = Produto;
