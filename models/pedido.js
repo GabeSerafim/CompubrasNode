7f6b2429e959b2272400f1163b9a304b5bdd8c0d
gSerafim <gserafim@brazpine.com>
2019-12-04 17:54:41 -0300

    feat(models): pedido

---
+++ models/pedido.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../utils/database');
+
+const Pedido = sequelize.define('pedido',{
+    CodPedido: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true
+    },
+    PrazoEntrega: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    DataPedido:{
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    CodCliente:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    CodVendedor:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+})
