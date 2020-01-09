600fe30a09e886a84b2c2d14e6655d37e31d9974
gSerafim <gserafim@brazpine.com>
2020-01-09 16:42:16 -0300

    feat(routes): updating routes index

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router();
 const cliente = require('./cliente.routes');
+const itempedido = require('./itempedido.routes');
+const pedido = require('./pedido.routes');
+const produto = require('./produto.routes');
+const vendedor = require('./vendedor.routes');
 
-router.use('/cliente',cliente)
+router.use('/cliente',cliente);
+router.use('/itempedido', itempedido);
+router.use('/pedido', pedido);
+router.use('/produto', produto);
+router.use('/vendedor', vendedor);
 
 module.exports = router;
